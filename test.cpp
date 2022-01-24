565c0dc978f5562769de501c87fc2763f72b93b2
fade_away_shot <654042016@qq.com>
2022-01-24 17:53:33 +0800

    time cost

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "RuningTime.hpp"
+
+int main() {
+  //  Sleep(3000);
+    std::cout << "Hello, world!\n";
+   // Sleep(3000);
+    std::cout<<"end"<<std::endl;
+    return 1;
+}
